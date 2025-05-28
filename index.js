34caeae7e967c2778be201b9fc49273d4825cb6c
kssme <kimsang555510@gmail.com>
2025-05-28 23:59:28 +0900

    서버 파일 업로드

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Davao Poker 서버가 정상 작동 중입니다.');
+});
+
+app.listen(PORT, () => {
+  console.log(`서버가 포트 ${PORT}에서 실행 중입니다.`);
+});
+cd server
+npm init -y
+npm install express
